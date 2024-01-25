29186e9719fcabc5367d100334c1f26274b464a1
Kveta Mooney <132582410+selfcoder-gazell@users.noreply.github.com>
2024-01-25 14:29:49 +0000

    created files in an app folder

---
+++ nextjs-profile/app/lib/blogs.js
@@ -0,0 +1,21 @@
+const blogs = [
+    {
+blog_id: blogs[0].id,
+date: '2024-01-25',
+title: 'My first blog',
+text: '1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+    }, 
+
+    {
+blog_id: blogs[1].id,
+date: '2024-01-26',
+title: 'My second blog',  
+text: '2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+    },
+    {
+blog_id: blogs[2].id,
+date: '2024-01-26',
+title: 'My third blog',  
+text: '4 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+    }
+];
